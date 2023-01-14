2b36f7d54c904e84c249d4520677d709e54988f3
Adminatio <86121233+Adminatio@users.noreply.github.com>
2023-01-14 14:54:47 +0530

    Create voting.py

---
+++ voting.py
@@ -0,0 +1,7 @@
+#To show whether the person is eligible fo vote or not with respective of their names.
+a=input("Enter the name:")
+b=int(input("Enter the age of the person:"))
+if b>=18:
+ print("Yes",a," Eligible for vote at age",b)
+else:
+ print("No",a,"Not legible for vote at age",b)
